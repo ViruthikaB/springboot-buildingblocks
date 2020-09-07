bb239fad35cc4f0c5f308cf686ea3db0c83b3dca
Viruthika <bviruthika@gmail.com>
2020-09-08 00:24:36 +0530

    Second commit

--- src/main/java/com/stacksimplify/restservices/SprigbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SprigbootBuildingblocksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SprigbootBuildingblocksApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(SprigbootBuildingblocksApplication.class, args);
 	}
